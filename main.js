7c035d4c44b8efcc2e46eaf1471e9c6bbb436a0d
Shriyans2023 <89887044+Shriyans2023@users.noreply.github.com>
2021-12-21 19:21:23 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+song="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload() {
+song = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video,0,0,600,500);
+}
+
+function play()
+{
+song.play();
+song.setVolume(1);
+song.rate(1);
+}
+
+function modelLoaded(){
+    console.log('Posenet is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length>0)
+    {
+        console.log(results)
+        leftWristX = results[0].poses.leftWrist.x;
+        leftWristY = results[0].poses.leftWrist.y;
+        console.log("leftWristX="+ leftWristY + "leftWristY="+ leftWristY)
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.Y;
+        console.log("rightWristX="+ rightWristY + "rightWristY="+ rightWristY)
+    }
+}
